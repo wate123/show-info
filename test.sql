5a9687d02dc2fbf689af52bf03ec2b4a25edd0ec
jun lin <junlin@juns-iMac.local>
2017-06-16 03:07:48 -0500

    Able to create table and charts

--- test.sql
+++ test.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 31, 2017 at 12:31 AM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Host: localhost
+-- Generation Time: Jun 16, 2017 at 10:06 AM
+-- Server version: 10.1.22-MariaDB
+-- PHP Version: 7.1.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,10 +25,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `table1`
+-- Table structure for table `imageDB`
 --
 
-CREATE TABLE `table1` (
+CREATE TABLE `imageDB` (
   `id` int(50) NOT NULL,
   `image` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -36,9 +38,9 @@ CREATE TABLE `table1` (
 --
 
 --
--- Indexes for table `table1`
+-- Indexes for table `imageDB`
 --
-ALTER TABLE `table1`
+ALTER TABLE `imageDB`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -46,10 +48,11 @@ ALTER TABLE `table1`
 --
 
 --
--- AUTO_INCREMENT for table `table1`
+-- AUTO_INCREMENT for table `imageDB`
 --
-ALTER TABLE `table1`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+ALTER TABLE `imageDB`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
