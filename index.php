b2f898195cfa1483dbfbd3cdd45eeef3d7a76036
wate123 <junlin0604@gmail.com>
2017-06-26 14:53:29 -0500

    add youtube analatics

--- index.php
+++ index.php
@@ -25,6 +25,16 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <link type="text/css" rel="stylesheet" href="index.css">
+    <script type="text/javascript" src="//www.google.com/jsapi"></script>
+    <script type="text/javascript" src="index.js"></script>
+    <script type="text/javascript" src="https://apis.google.com/js/client.js?onload=onJSClientLoad"></script>
+
+    <!-- load the Google API Client Libraries -->
+
+
+
     <script src="hot.js"></script>
     <script src="chart.js"></script>
     <!-- Load the AJAX API -->
@@ -130,6 +140,7 @@
         <ul class="nav nav-tabs" role="tablist" id="myTab">
           <li role="presentation" class="active"><a href="#image" aria-controls="image" data-toggle="tab"  >Image</a></li>
           <li role="presentation"><a href="#table" aria-controls="table"data-toggle="tab">Table and Chart</a></li>
+          <li role="presentation"><a href="#youtube" aria-controls="youtube"data-toggle="tab">Youtube Report</a></li>
           <!-- <li role="presentation"><a href="#text" aria-controls="text"data-toggle="tab">Text</a></li> -->
         </ul>
 
@@ -220,6 +231,110 @@
                 </div>
             </div>
               </div>
+        </div>
+        <div  class="tab-pane fade" id="youtube">
+
+
+          <div id="login-container" class="pre-auth">This application requires access to your YouTube account.
+            Please <a href="#" id="login-link">authorize</a> to continue.
+          </div>
+      		  <div class="post-auth">
+      			<div id="message"></div>
+      			<div id="chart"></div>
+      			<div>Choose a Video:</div>
+      			<ul id="video-list"></ul>
+      		  </div>
+
+            <script>
+            var GoogleAuth;
+            var SCOPE = 'https://www.googleapis.com/auth/yt-analytics.readonly';
+            function handleClientLoad() {
+              // Load the API's client and auth2 modules.
+              // Call the initClient function after the modules load.
+              gapi.load('client:auth2', initClient);
+            }
+
+            function initClient() {
+              // Retrieve the discovery document for version 1 of YouTube Analytics API.
+              // In practice, your app can retrieve one or more discovery documents.
+              var discoveryUrl = 'https://www.googleapis.com/discovery/v1/apis/youtubeAnalytics/v1/rest';
+
+              // Initialize the gapi.client object, which app uses to make API requests.
+              // Get API key and client ID from API Console.
+              // 'scope' field specifies space-delimited list of access scopes.
+              gapi.client.init({
+                  'apiKey': 'AIzaSyDW0mGUK2oyfjTQSJCXxJ9kXLvIVunCyH4',
+                  'discoveryDocs': [discoveryUrl],
+                  'clientId': '975363526269-thnopg16l9fivqf45u5t1nehp4s17e52.apps.googleusercontent.com',
+                  'scope': SCOPE
+              }).then(function () {
+                GoogleAuth = gapi.auth2.getAuthInstance();
+
+                // Listen for sign-in state changes.
+                GoogleAuth.isSignedIn.listen(updateSigninStatus);
+
+                // Handle initial sign-in state. (Determine if user is already signed in.)
+                var user = GoogleAuth.currentUser.get();
+                setSigninStatus();
+
+                // Call handleAuthClick function when user clicks on
+                //      "Sign In/Authorize" button.
+                $('#sign-in-or-out-button').click(function() {
+                  handleAuthClick();
+                });
+                $('#revoke-access-button').click(function() {
+                  revokeAccess();
+                });
+              });
+            }
+
+            function handleAuthClick() {
+              if (GoogleAuth.isSignedIn.get()) {
+                // User is authorized and has clicked 'Sign out' button.
+                GoogleAuth.signOut();
+              } else {
+                // User is not signed in. Start Google auth flow.
+                GoogleAuth.signIn();
+              }
+            }
+
+            function revokeAccess() {
+              GoogleAuth.disconnect();
+            }
+
+            function setSigninStatus(isSignedIn) {
+              var user = GoogleAuth.currentUser.get();
+              var isAuthorized = user.hasGrantedScopes(SCOPE);
+              if (isAuthorized) {
+                $('#sign-in-or-out-button').html('Sign out');
+                $('#revoke-access-button').css('display', 'inline-block');
+                $('#auth-status').html('You are currently signed in and have granted ' +
+                    'access to this app.');
+              } else {
+                $('#sign-in-or-out-button').html('Sign In/Authorize');
+                $('#revoke-access-button').css('display', 'none');
+                $('#auth-status').html('You have not authorized this app or you are ' +
+                    'signed out.');
+              }
+            }
+
+            function updateSigninStatus(isSignedIn) {
+              setSigninStatus();
+            }
+          </script>
+
+          <button id="sign-in-or-out-button"
+                  style="margin-left: 25px">Sign In/Authorize</button>
+          <button id="revoke-access-button"
+                  style="display: none; margin-left: 25px">Revoke access</button>
+
+          <div id="auth-status" style="display: inline; padding-left: 25px"></div><hr>
+
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+          <script async defer src="https://apis.google.com/js/api.js"
+                  onload="this.onload=function(){};handleClientLoad()"
+                  onreadystatechange="if (this.readyState === 'complete') this.onload()">
+          </script>
         </div>
       </div>
       <!-- Keep the current tab when refreash -->
